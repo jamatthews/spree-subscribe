92ad9e8fcd3f7731e1a424931de1c2a5cff70f48
Jacob Matthews <jake@hell.co.nz>
2015-09-14 21:57:58 +1200

    should actually use master

--- app/helpers/spree/subscriptions_helper.rb
+++ app/helpers/spree/subscriptions_helper.rb
@@ -7,7 +7,8 @@ module Spree::SubscriptionsHelper
     if product.variant
       Spree::Money.new product.variant.price - product.variant.subscribed_price
     else
-      Spree::Money.new product.price - product.subscribed_prices
+      Spree::Money.new product.master.price - product.master.subscribed_price
+    end
   end
 
   def subscription_price_difference_for_line_item(line_item)
