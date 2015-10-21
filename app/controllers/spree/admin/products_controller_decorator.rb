fef886b89844713ed559a657a8d4293493b20879
Jacob Matthews <jake@hell.co.nz>
2015-10-21 19:10:55 +1300

    make admin stuff optional if only running frontend

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,12 +1,14 @@
-Spree::Admin::ProductsController.class_eval do
-  before_filter :check_subscription_intervals, :only => [:update]
+if defined? Spree::Admin::ProductsController
+  Spree::Admin::ProductsController.class_eval do
+    before_filter :check_subscription_intervals, :only => [:update]
 
-  protected
+    protected
 
-  def check_subscription_intervals
-    if params[:product][:subscription_interval_ids].present?
-      params[:product][:subscription_interval_ids] = params[:product][:subscription_interval_ids].split(',')
+    def check_subscription_intervals
+      if params[:product][:subscription_interval_ids].present?
+        params[:product][:subscription_interval_ids] = params[:product][:subscription_interval_ids].split(',')
+      end
     end
-  end
 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Spree::Core::Engine.routes.draw do
-  namespace :admin do
-    resources :subscription_intervals do
-      collection do
-        get :search
+  
+  if defined? Spree::Admin::ProductsController
+    namespace :admin do
+      resources :subscription_intervals do
+        collection do
+          get :search
+        end
       end
+      resources :subscriptions, :except => [:new,:create]
     end
-    resources :subscriptions, :except => [:new,:create]
   end
-
+  
   resources :subscriptions, :only => [:destroy]
 
 end
