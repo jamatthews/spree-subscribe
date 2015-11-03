98c39675bd0dc1d4ce78a4d6d2c7c650a8b504a4
Jacob Matthews <jake@hell.co.nz>
2015-11-04 00:03:37 +1300

    only enable admin controllers on backend

--- app/controllers/spree/admin/subscription_intervals_controller.rb
+++ app/controllers/spree/admin/subscription_intervals_controller.rb
@@ -1,15 +1,17 @@
-class Spree::Admin::SubscriptionIntervalsController < Spree::Admin::ResourceController
-  respond_to :html, :json, :js
+if defined? Spree::Admin::ProductsController
+  class Spree::Admin::SubscriptionIntervalsController < Spree::Admin::ResourceController
+    respond_to :html, :json, :js
 
-  def search
-    if params[:ids]
-      @intervals = Spree::SubscriptionInterval.where(:id => params[:ids].split(','))
-    else
-      @intervals = Spree::SubscriptionInterval.limit(20).search(:name_cont => params[:q]).result
+    def search
+      if params[:ids]
+        @intervals = Spree::SubscriptionInterval.where(:id => params[:ids].split(','))
+      else
+        @intervals = Spree::SubscriptionInterval.limit(20).search(:name_cont => params[:q]).result
+      end
     end
-  end
 
-  def subscription_interval_params
-    params.permit(:ids, :q)
+    def subscription_interval_params
+      params.permit(:ids, :q)
+    end
   end
 end
--- app/controllers/spree/admin/subscriptions_controller.rb
+++ app/controllers/spree/admin/subscriptions_controller.rb
@@ -1,26 +1,28 @@
-class Spree::Admin::SubscriptionsController < Spree::Admin::ResourceController
-  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-  before_action :load_subscription, except: :index
+if defined? Spree::Admin::ProductsController
+  class Spree::Admin::SubscriptionsController < Spree::Admin::ResourceController
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+    before_action :load_subscription, except: :index
 
-  helper "spree/subscriptions"
+    helper "spree/subscriptions"
 
-  def index
-    @subscriptions = Spree::Subscription.where(state: [:active, :inactive]).
-      page(params[:page]).
-      per(params[:per_page] || Spree::Config[:orders_per_page])
+    def index
+      @subscriptions = Spree::Subscription.where(state: [:active, :inactive]).
+        page(params[:page]).
+        per(params[:per_page] || Spree::Config[:orders_per_page])
+    end
+
+    def destroy
+      @subscription.active? ? @subscription.suspend : @subscription.resume
+
+      redirect_to account_url
+    end
+
+    private
+
+    def load_subscription
+      @subscription = Spree::Subscription.find_by_id! params[:id]
+      authorize! action, @subscription
+    end
+
   end
-
-  def destroy
-    @subscription.active? ? @subscription.suspend : @subscription.resume
-
-    redirect_to account_url
-  end
-
-  private
-
-  def load_subscription
-    @subscription = Spree::Subscription.find_by_id! params[:id]
-    authorize! action, @subscription
-  end
-
 end
