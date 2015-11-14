6258322a6e258fdda6c956af24a0623639758dce
Jacob Matthews <jake@hell.co.nz>
2015-11-15 11:02:19 +1300

    resuce nil on subscribed price

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,11 +2,15 @@ Spree::BaseHelper.class_eval do
   
   def display_price(product_or_variant)
     price = product_or_variant.price_in(current_currency).price
-    display_price = number_to_currency(price, precision: (price.round == price ? 0 : 2))
+    number_to_currency(price, precision: (price.round == price ? 0 : 2))
   end
   
   def subscription_display_price(product_or_variant)
-    price = product_or_variant.subscribed_price
-    display_price = number_to_currency(price, precision: (price.round == price ? 0 : 2))
+    if product_or_variant.subscribed_price.present?
+      price = product_or_variant.subscribed_price
+      return number_to_currency(price, precision: (price.round == price ? 0 : 2))
+    else
+      display_price
+    end  
   end
 end
